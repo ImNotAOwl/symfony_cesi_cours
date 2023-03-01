445e1f111210a0c3cb395fa16b8311df247ee7e9
MattSiegel <siegel.matthieu@ymail.com>
2023-03-01 14:45:19 +0100

    add user roles

---
+++ src/Form/UserType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('isVerified')
+        ;
+
+        $builder->add('roles', ChoiceType::class, [
+            'choices'  => [
+                "Utilisateur" => "ROLE_USER",
+                'Admin assos' => "ROLE_ADMIN_ASSOS",
+                'Admin entreprises' => "ROLE_ADMIN_ENTREPRISES",
+                'Manager' => "ROLE_MANAGER",
+                'Sup admin' => "ROLE_SUP_ADMIN",
+            ],
+        ]);
+        
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesAsArray) {
+                    // transform the array to a string
+                    return implode(', ', $rolesAsArray);
+                },
+                function ($rolesAsString) {
+                    // transform the string back to an array
+                    return explode(', ', $rolesAsString);
+                }
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
